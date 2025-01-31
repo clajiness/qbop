93bdb13858654d97e4b54d4aa1ce04248a85fdb6
Chad Lajiness <chad.lajiness@icloud.com>
2025-01-30 23:04:27 -0600

    fixed select queries

--- service/counter.rb
+++ service/counter.rb
@@ -2,23 +2,23 @@ module Service
   # tracks the number of attempts to change the port in opnsense and qBit
   class Counter
     def required_attempts
-      db_execute('select required_attempts from counters where id = 1')
+      db_execute('select required_attempts from counters where id = 1').first
     end
 
     def opnsense_attempt
-      db_execute('select opnsense_attempt from counters where id = 1')
+      db_execute('select opnsense_attempt from counters where id = 1').first
     end
 
     def qbit_attempt
-      db_execute('select qbit_attempt from counters where id = 1')
+      db_execute('select qbit_attempt from counters where id = 1').first
     end
 
     def opnsense_change
-      db_execute('select opnsense_change from counters where id = 1')
+      db_execute('select opnsense_change from counters where id = 1').first
     end
 
     def qbit_change
-      db_execute('select qbit_change from counters where id = 1')
+      db_execute('select qbit_change from counters where id = 1').first
     end
 
     def increment_opnsense_attempt
