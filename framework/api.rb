0114b8fff5f79c29cb34bd8381292d5c6c244872
Chad Lajiness <chad.lajiness@icloud.com>
2025-02-01 16:18:12 -0600

    Cleaned up class structure and fixed Stats class typo

--- framework/api.rb
+++ framework/api.rb
@@ -1,25 +1,27 @@
-class API < Grape::API # rubocop:disable Style/Documentation
-  format :json
-  prefix :api
+module Framework
+  class API < Grape::API # rubocop:disable Style/Documentation
+    format :json
+    prefix :api
 
-  get '/stats' do
-    SQLite3::Database.new 'data/prod.db' do |db|
-      db.results_as_hash = true
-      @stats = db.execute('select * from stats where id = 1').first
+    get '/stats' do
+      SQLite3::Database.new 'data/prod.db' do |db|
+        db.results_as_hash = true
+        @stats = db.execute('select * from stats where id = 1').first
+      end
+
+      { 'stats' => {
+        'protonvpn' => {
+          'current_port': @stats['proton_current_port']
+        },
+        'opnsense' => {
+          'current_port': @stats['opn_current_port'],
+          'updated_at': @stats['opn_updated_at']
+        },
+        'qbit' => {
+          'current_port': @stats['qbit_current_port'],
+          'updated_at': @stats['qbit_updated_at']
+        }
+      } }
     end
-
-    { 'stats' => {
-      'protonvpn' => {
-        'current_port': @stats['proton_current_port']
-      },
-      'opnsense' => {
-        'current_port': @stats['opn_current_port'],
-        'updated_at': @stats['opn_updated_at']
-      },
-      'qbit' => {
-        'current_port': @stats['qbit_current_port'],
-        'updated_at': @stats['qbit_updated_at']
-      }
-    } }
   end
 end
--- framework/web.rb
+++ framework/web.rb
@@ -1,32 +1,34 @@
-class Web < Sinatra::Application # rubocop:disable Style/Documentation
-  get '/' do
-    SQLite3::Database.new 'data/prod.db' do |db|
-      db.results_as_hash = true
-      @stats = db.execute('select * from stats where id = 1').first
+module Framework
+  class Web < Sinatra::Application # rubocop:disable Style/Documentation
+    get '/' do
+      SQLite3::Database.new 'data/prod.db' do |db|
+        db.results_as_hash = true
+        @stats = db.execute('select * from stats where id = 1').first
+      end
+
+      erb :index
     end
 
-    erb :index
-  end
+    get '/logs' do
+      log_lines = ENV['LOG_LINES'] || 50
 
-  get '/logs' do
-    log_lines = ENV['LOG_LINES'] || 50
+      output = []
+      File.readlines('data/log/qbop.log').last(log_lines.to_i).each do |line|
+        output << line
+      end
+      @output = output
+      @log_lines = log_lines
 
-    output = []
-    File.readlines('data/log/qbop.log').last(log_lines.to_i).each do |line|
-      output << line
+      erb :logs
     end
-    @output = output
-    @log_lines = log_lines
 
-    erb :logs
-  end
+    get '/about' do
+      @repo_url = 'https://github.com/clajiness/qbop'
+      @script_version = ENV['VERSION']
+      @ruby_version = "#{RUBY_VERSION} (p#{RUBY_PATCHLEVEL})"
+      @uptime = `uptime -p`.strip
 
-  get '/about' do
-    @repo_url = 'https://github.com/clajiness/qbop'
-    @script_version = ENV['VERSION']
-    @ruby_version = "#{RUBY_VERSION} (p#{RUBY_PATCHLEVEL})"
-    @uptime = `uptime -p`.strip
-
-    erb :about
+      erb :about
+    end
   end
 end
